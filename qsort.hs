fe07cfa580771fb59fdda9d38a2fe6587c8b1343
Wouter Willaert <wouter@typework.com>
2014-12-15 20:48:31 +0100

    update qsort

--- qsort.hs
+++ qsort.hs
@@ -1,4 +1,8 @@
-qsort = []
+module QuickSort where
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
 qsort (x : xs) = qsort smaller ++ [x] ++ qsort larger
-  where smaller [a | a <- xs, a <= x]
-        larger [b | b <- xs, b > a]
+    where
+      smaller = [a | a <- xs, a <= x]
+      larger = [b | b <- xs, b > x]
