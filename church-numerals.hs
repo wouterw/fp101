d157ea4019b8539ab7fd29407ee73f6f2018e7d1
Wouter Willaert <wouter@typework.com>
2014-12-15 20:48:48 +0100

    update church numerals

--- church-numerals.hs
+++ church-numerals.hs
@@ -1,3 +1,5 @@
+module ChurchNumerals where
+
 type Church = (a -> a) -> a -> a
 
 curchToInteger x = x (+1) 0
@@ -6,5 +8,6 @@ curchToInteger two = (\s z -> s (s z)) (+1) 0
 curchToString x = x ('*':) ""
 curchToString two = (\s z -> s (s z)) ('*':) ""
 
-add x y = \s z -> x s (y s z)
-mul x y = \s z -> x (y s) z
+cAdd x y = \s z -> x s (y s z)
+cMul x y = \s z -> x (y s) z
+cExp (CNat) x (CNat) y = CNat (b a)
